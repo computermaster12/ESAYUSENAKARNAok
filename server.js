7a73054379a021c621b14d40a2fbde1412e287fe
computermaster12 <rehbartech1@gmail.com>
2025-03-28 21:19:48 +0500

    Update server.js

--- server.js
+++ server.js
@@ -214,7 +214,7 @@ app.get('/api/history', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 8082;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
